85d2db24d5857fc288ff96776de076cf3f91ed01
Stefano Scalfari <vorshim92@gmail.com>
2025-12-17 23:06:09 +0100

    fix name

--- Contents/mods/MapCordCursor/common/media/lua/client/Map Cordinates Cursor/ISWorldMapCursorCord.lua
+++ Contents/mods/MapCordCursor/common/media/lua/client/Map Cordinates Cursor/ISWorldMapCursorCord.lua
@@ -10,7 +10,7 @@ if getActivatedMods():contains("\\MGRS (FMCCYAYFGLE)") then
 end
 
 local MDZ = false
-if getActivatedMods():contains("\\MoreDifficultZones") or getActivatedMods():contains("\\TierZonyne") then
+if getActivatedMods():contains("\\MoreDifficultZones") or getActivatedMods():contains("\\TierZonyne42") then
     MDZ = true
 end
 
